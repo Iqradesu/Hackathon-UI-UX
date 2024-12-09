5953a1d12a2d85187338fd88284eb90a2db564b0
Iqradesu <iqraghaffar420@gmail.com>
2024-12-09 12:56:59 +0500

    initial commit

--- src/app/(pages)/Shop/page.tsx
+++ src/app/(pages)/Shop/page.tsx
@@ -1,9 +1,9 @@
-import Header from './Components/Header'
+import Header from '../../Components/Header'
 import React from 'react'
-import Navbar from './Components/Navbar'
-import ItemList from './Components/Shop'
-import Footer from './Components/Footer'
-import FeatureSection from './Components/FeatureSection'
+import Navbar from '../../Components/Navbar'
+import ItemList from '../../Components/Shop'
+import Footer from '../../Components/Footer'
+import FeatureSection from '../../Components/FeatureSection'
 import { IoMdSwitch } from "react-icons/io";
 import { TbGridDots } from "react-icons/tb";
 const about = () => {
